38c12796e41a8c7c20ae6c40b5070f1d8dce9f22
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-05-22 23:36:10 +0100

    registering contact blueprint

--- HMS/app.py
+++ HMS/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from hms.blueprints.page import page 
-
+from hms.blueprints.contact import contact
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
@@ -8,5 +8,6 @@ def create_app():
     app.config.from_object('config.settings')
 
     app.register_blueprint(page)
+    app.register_blueprint(contact)
 
     return app
