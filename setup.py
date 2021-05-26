2d09e80da595dcae81b6f43a3d12fd9fe0fdd37a
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-05-26 22:34:31 +0100

    adding set up file

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+# setup(
+#     name='HMS-CLI',
+#     version='1.0',
+#     packages=['cli', 'cli.commands'],
+#     include_package_data=True,
+#     install_requires=[
+#         'click',
+#     ],
+#     entry_points="""
+#         [console_scripts]
+#         hms=cli.cli:cli
+#     """,
+# )
+
+setup(
+    name='HMS-CLI',
+    version='1.0.0',
+    packages=['cli', 'cli.commands'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+    entry_points="""
+        [console_scripts]
+        hms=cli.cli:cli
+    """ 
+)
