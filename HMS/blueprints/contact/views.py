f70d46c0f9dc805179dcdc63c3cb10dde81aa15b
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-05-26 21:59:31 +0100

    adding contact function

--- HMS/blueprints/contact/views.py
+++ HMS/blueprints/contact/views.py
@@ -1,6 +1,14 @@
 from hms.blueprints.contact import contact
 from flask import render_template
+from hms.blueprints.contact.forms import ContactForm
 
 @contact.route('/visitor')
 def contact():
-    return render_template('contact/contact.html')
+    form = ContactForm()
+
+    if form.validate_on_submit():
+        email = form.email.data
+        message = form.message.data
+        name= form.name.data
+        subject = form.subject.data
+    return render_template('contact/contact.html', form=form)
