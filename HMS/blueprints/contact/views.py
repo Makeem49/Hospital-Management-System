7d78918159ab290de4a91c34a649c87081b2aa08
Mosood Akeem <moshoodakeemolayinka@gmail.com>
2021-05-27 22:24:14 +0100

    importing the send email function

--- HMS/blueprints/contact/views.py
+++ HMS/blueprints/contact/views.py
@@ -2,13 +2,20 @@ from hms.blueprints.contact import contact
 from flask import render_template
 from hms.blueprints.contact.forms import ContactForm
 
-@contact.route('/visitor')
-def contact():
-    form = ContactForm()
 
+@contact.route('/contact', methods=['GET', 'POST'])
+def index():
+    form = ContactForm()
     if form.validate_on_submit():
         email = form.email.data
         message = form.message.data
         name= form.name.data
         subject = form.subject.data
+        data = {
+            'message' : message,
+            'name' : name,
+            'email' : email
+        }
+        from hms.blueprints.contact.tasks import contact_task
+        contact_task(email, subject, 'contact/email/index', data)
     return render_template('contact/contact.html', form=form)
