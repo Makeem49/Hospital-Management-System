b369787e541079551ce0ec35401dab3e5b91f205
Mosood Akeem <moshoodakeemolayinka@gmail.com>
2021-05-27 22:29:14 +0100

    modifying contact

--- HMS/blueprints/contact/__init__.py
+++ HMS/blueprints/contact/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint 
 
-contact = Blueprint('contact', __name__, template_folder='templates', url_prefix='/contact')
+contact = Blueprint('contact', __name__, template_folder='templates')
 
 from hms.blueprints.contact import views
