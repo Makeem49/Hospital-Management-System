6bab6b636c094046e2265101d0ab0031bc56ef09
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-06-04 03:22:32 +0100

    adding CSRFprotect

--- HMS/extensions.py
+++ HMS/extensions.py
@@ -2,9 +2,11 @@ from flask_mail import Mail
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_wtf import CsrfProtect
 from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy
+from flask_wtf.csrf import CSRFProtect
+
 
 mail = Mail()
 debugger = DebugToolbarExtension()
 csrf = CsrfProtect()
 db = SQLAlchemy()
+csrf = CSRFProtect()
