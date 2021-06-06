414ea4b46f30c5668060d4de49110af08de69dec
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-06-06 23:14:18 +0100

    Removing a depreciated class from flask_wtf

--- HMS/extensions.py
+++ HMS/extensions.py
@@ -2,11 +2,11 @@ from flask_mail import Mail
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_wtf import CsrfProtect
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf.csrf import CSRFProtect
+from flask_wtf.csrf import  CsrfProtect
 
 
 mail = Mail()
 debugger = DebugToolbarExtension()
 csrf = CsrfProtect()
 db = SQLAlchemy()
-csrf = CSRFProtect()
+csrf = CsrfProtect()
