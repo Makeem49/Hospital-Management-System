486962cdacd652a32c2d5c748b005f8d5ed5cb57
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-06-16 22:55:52 +0100

    Creating creating function route

--- HMS/blueprints/contact/views.py
+++ HMS/blueprints/contact/views.py
@@ -1,26 +1,28 @@
-import flask
+from flask import flash, redirect, render_template, url_for, request
 from hms.blueprints.contact import contact
-from flask import render_template, redirect, url_for, flash 
 from hms.blueprints.contact.forms import ContactForm
+from hms.email import contact_me
 
 
 @contact.route('/contact', methods=['GET', 'POST'])
 def index():
     form = ContactForm()
     if form.validate_on_submit():
+        print(form.email.data)
+        name = form.email.data
         email = form.email.data
         message = form.message.data
-        first_name= form.first_name.data
-        last_name = form.last_name.data
-        subject = form.subject.data
+
         data = {
-            'message' : message,
-            'first_name' : first_name,
-            'last_name' : last_name,
-            'email' : email
+            'name' : name,
+            'email' : email,
+            'message' : message
         }
-        from hms.blueprints.contact.tasks import contact_task
-        contact_task(email, subject, 'contact/email/index', data)
-        flash("Message has been successfully delivered, you'll get response soon.", "success")
-        return redirect(url_for('page.home'))
-    return render_template('contact/contact.html', form=form)
+
+        contact_me(email, "Contact message.", '/contact/mail/index', data=data)
+        flash("Thank you for contacting us, your message has been delivered.", 'success')
+        return redirect(url_for("page.home"))
+    return render_template('contact/index.html', form = form )
+
+
+
