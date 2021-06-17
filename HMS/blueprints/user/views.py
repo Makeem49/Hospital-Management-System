7af71606dc352ba1a6f1b58dc4cb5c41a2b7744e
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-06-17 03:37:21 +0100

    Modifying contact views

--- HMS/blueprints/user/views.py
+++ HMS/blueprints/user/views.py
@@ -1,5 +1,6 @@
+from flask.templating import render_template
 from hms.blueprints.user import user
 
 @user.route('/register', methods=['GET', 'POST'])
 def register():
-    pass
+    return render_template('page/register.html')
