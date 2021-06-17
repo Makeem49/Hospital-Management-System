6e32d2c611bf9f440b497e27b0aeff6fb3afe56d
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-06-17 03:36:32 +0100

    Adding threading to mail function

--- HMS/email.py
+++ HMS/email.py
@@ -1,10 +1,18 @@
 from hms.extensions import mail 
-from flask import app, current_app, render_template
+from flask import current_app, render_template
 from flask_mail import Message
+from threading import Thread
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def contact_me(email, subject, template, **kwargs):
     app = current_app._get_current_object()
     msg = Message(sender=email, recipients=app.config['MAIL_USERNAME'], reply_to=email, subject=subject)
     msg.body = render_template(template + '.txt' , **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+
 
