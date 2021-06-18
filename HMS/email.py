09a8142a20270f0117b41a10b4d4eb190c299eba
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-06-18 01:45:30 +0100

    resolving email function

--- HMS/email.py
+++ HMS/email.py
@@ -9,10 +9,11 @@ def send_async_email(app, msg):
 
 def contact_me(email, subject, template, **kwargs):
     app = current_app._get_current_object()
-    msg = Message(sender=email, recipients=app.config['MAIL_USERNAME'], reply_to=email, subject=subject)
+    msg = Message(sender=email, recipients=[app.config['MAIL_USERNAME']], reply_to=email, subject=subject)
     msg.body = render_template(template + '.txt' , **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr
+    mail.send(msg)
+    # thr = Thread(target=send_async_email, args=[app, msg])
+    # thr.start()
+    # return thr
 
 
