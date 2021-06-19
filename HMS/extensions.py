dab02d575fef0e1b7905e1bc6ad027e4b81a929a
Moshood Akeem <moshoodakeemolayinka@gmail.com>
2021-06-19 04:18:57 +0100

    Adding flask_login extension

--- HMS/extensions.py
+++ HMS/extensions.py
@@ -1,9 +1,11 @@
 from flask_mail import Mail
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_wtf import CsrfProtect
+from flask_login import LoginManager
 
 
 
 mail = Mail()
 debugger = DebugToolbarExtension()
 csrf = CsrfProtect()
+login_manager = LoginManager()
