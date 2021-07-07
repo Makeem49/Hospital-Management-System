21f5666e774f8813c3e4d0e8d274b1fcb6c1dab5
Mosood Akeem <moshoodakeemolayinka@gmail.com>
2021-07-07 02:37:59 +0100

    Adding token and confirmation token to models

--- HMS/blueprints/user/model.py
+++ HMS/blueprints/user/model.py
@@ -3,6 +3,8 @@ from hms.extensions import db
 from werkzeug import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hms.extensions import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 @login_manager.user_loader
 def load_user(id):
@@ -19,11 +21,11 @@ class User(UserMixin,db.Model):
     firstName = db.Column(db.String(32), nullable = False)
     lastName = db.Column(db.String(32), nullable = False)
     otherName = db.Column(db.String(32), nullable = True, server_default='')
-    date_or_birth = db.Column(db.DateTime(), nullable = False)
-    state_of_origin = db.Column(db.String(64), nullable=False)
-    place_of_birth = db.Column(db.String(64), nullable=False)
-    address = db.Column(db.String(120), nullable = False)
-    gender = db.Column(db.String(10), nullable = False)
+    date_or_birth = db.Column(db.DateTime(), nullable = True)
+    state_of_origin = db.Column(db.String(64), nullable=True)
+    place_of_birth = db.Column(db.String(64), nullable=True)
+    address = db.Column(db.String(120), nullable = True)
+    gender = db.Column(db.String(10), nullable = True)
 
     # Patient login credentials 
     username = db.Column(db.String(64), nullable = True, unique = True)
@@ -32,6 +34,7 @@ class User(UserMixin,db.Model):
 
     # Account status
     active = db.Column(db.Integer, server_default = '1', nullable = False)
+    confirmed = db.Column(db.Bolean, default = False)
 
 
     def __init__(self, **kwargs):
@@ -58,3 +61,73 @@ class User(UserMixin,db.Model):
     @classmethod
     def find_by_identity(cls, identity):
         return User.query.filter((User.email == identity ) | (User.username == identity)).first()
+
+    def serialize_confirmation_token(self, expiration=500):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'confirm' : self.id}).decode('utf-8')
+
+    def disserialize_confirmation_token(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.commit()
+
+        return User.query.get(data.get('confirm'))
+
+    def generate_reset_password(self, expiration=500):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'confirm' : self.id}).decode('utf-8')
+
+    @staticmethod
+    def reset_pasword(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        return User.query.get(data.get('confirm'))
+
+    def generate_email_token(self, email, expiration=500):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'confirm' : self.id, 'new_email' : email})
+
+    def confirm_email_token(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        
+        user = User.query.get(data.get('confirm'))
+        new_email = User.query.get(data.get('new_email'))
+
+        if data.get('confirm') != self.id:
+            return False
+
+        if User.query.filter_by(email = new_email ).first():
+            return False
+
+        if user is None:
+            return False
+
+        user.email = new_email
+        db.session.add(self)
+
+        return True
+        
+
+
+    
+
+
+        
+
+        
